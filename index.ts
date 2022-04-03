7429baec98b4ab79496720b0ecfeb512f5224eeb
guygreenleaf <guygreenleaf@icloud.com>
2022-04-03 14:49:27 -0700

    move to heroku deploy

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import { devicesRouter } from "./routes/devices.router";
 
 const app = express();
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 connectToDatabase()
     .then(() => {
